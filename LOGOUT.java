dd5ab8e94b2dc1bf14114682349310e9c072696e
farhanah <farhanahsaid05@gmail.com>
2024-11-29 16:05:39 +0800

    Add files via upload

---
+++ LOGOUT.java
@@ -0,0 +1,26 @@
+
+package assignment;
+
+public class LOGOUT {
+ switch (choice) {
+            case 7:
+                logout();
+                break;
+            default:
+                System.out.println("Invalid option. Please try again.");
+        }
+    }
+    private static void logout() {
+    // clear the user name session
+        name = null; // set the current user name to null
+        System.out.println("Thank you for using \"Ledger System (name can created yourself)\" ");
+    }
+
+
+
+
+
+
+
+
+
